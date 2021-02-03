bc9054d1664242793ff7f443591695470ab42fcf
N17430 <khjoon0204@interpark.com>
2021-02-03 12:44:55 +0900

    프로젝트 생성

---
+++ AppCard/AppCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppCard
+//
+//  Created by N17430 on 2021/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
